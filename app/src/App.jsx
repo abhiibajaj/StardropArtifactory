562cc8a9b98513f3018e4144ca1679434c293f95
Matthew Littleson <mlittleson@student.unimelb.edu.au>
2019-09-09 16:15:12 +1000

    added Sign in Page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import HomePage from './components/HomePage'
 import LandingPage from './components/LandingPage'
+import SignInPage from './components/SignInPage'
 import SignUpPage from './components/SignUpPage'
 import Navbar from './components/Navbar'
 import AuthRoute from './components/AuthRoute'
@@ -12,6 +13,7 @@ function App() {
       <Router>
         <Navbar />
         <Route path="/" exact component={LandingPage} />
+        <Route path="/signin" component={SignInPage} />
         <Route path="/signup" component={SignUpPage} />
         <AuthRoute path='/home'>
           <HomePage />
---
+++ app/src/components/SignInPage/EmailPasswordSignIn.jsx
@@ -0,0 +1,81 @@
+import React from 'react'
+import { Form, Button } from 'react-bootstrap'
+import Loading from '../Loading'
+import { Redirect } from 'react-router-dom'
+
+export default class EmailPasswordSignUp extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      password: '',
+      error: '',
+      loading: false,
+      redirectNow: false,
+    }
+  }
+
+  handleSubmit = event => {
+    event.preventDefault()
+    // get email and password from state
+    const { email, password } = this.state
+    this.setState({ loading: true })
+    // sign user in
+    this.props.firebase
+      .auth
+      .signInWithEmailAndPassword(email, password)
+      .then(() => setTimeout(() =>
+        // gotta use a timeout because firebase is still loading 🤔
+        this.setState({ loading: false, redirectNow: true }),
+        1000
+      ))
+      .catch(error => this.setState({ error: error.message }))
+  }
+
+  handleChange = event => {
+    // use the name field as the index for the object
+    this.setState({ [event.target.name]: event.target.value })
+  }
+
+  render() {
+    const { email, password, error, loading, redirectNow } = this.state
+    return (
+
+      <Form onSubmit={this.handleSubmit}>
+
+        {error}
+        {loading ? <Loading /> : ''}
+        {redirectNow ? <Redirect to="/home" /> : ''}
+
+        <Form.Group controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            name="email"
+            value={email}
+            onChange={this.handleChange}
+            autoComplete="email"
+            required
+          />
+        </Form.Group>
+
+        <Form.Group controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={this.handleChange}
+            autoComplete="password"
+            required
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+      </Button>
+      </Form>
+    )
+  }
+}
