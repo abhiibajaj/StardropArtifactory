c77a0c3399a57506a8d316ecc953325b2509810d
Arpit <arpitbajaj98@gmail.com>
2019-09-22 22:45:23 +1000

    Added route to the add artifact page

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,26 +1,25 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import HomePage from './components/HomePage'
-import LandingPage from './components/LandingPage'
-import SignInPage from './components/SignInPage'
-import SignUpPage from './components/SignUpPage'
-import Navbar from './components/Navbar'
-import AuthRoute from './components/AuthRoute'
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import HomePage from "./components/HomePage";
+import LandingPage from "./components/LandingPage";
+import SignInPage from "./components/SignInPage";
+import SignUpPage from "./components/SignUpPage";
+import Navbar from "./components/Navbar";
+import AuthRoute from "./components/AuthRoute";
 
 function App() {
-  return (
-    <div>
-      <Router>
-        <Navbar />
-        <Route path="/" exact component={LandingPage} />
-        <Route path="/signin" component={SignInPage} />
-        <Route path="/signup" component={SignUpPage} />
-        <AuthRoute path='/home' component={HomePage} />
-      </Router>
-    </div>
-  )
+    return (
+        <div>
+            <Router>
+                <Navbar />
+                <Route path="/" exact component={LandingPage} />
+                <Route path="/addartifact" component={AddArtifactPage} />
+                <Route path="/signin" component={SignInPage} />
+                <Route path="/signup" component={SignUpPage} />
+                <AuthRoute path="/home" component={HomePage} />
+            </Router>
+        </div>
+    );
 }
 
-
-
-export default App
+export default App;
