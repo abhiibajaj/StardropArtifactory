d91e07a50374ab06920b8ba53b7beb9d6beb1b0a
Arpit <arpitbajaj98@gmail.com>
2019-09-23 02:35:50 +1000

    Initial functionality for viewing all artifacts on homepage

--- app/src/components/HomePage/index.jsx
+++ app/src/components/HomePage/index.jsx
@@ -1,9 +1,39 @@
-import React from 'react'
+import React from "react";
+import withFirebase from "../../contexts/withFirebase";
 
-export default class HomePage extends React.Component {
-  render() {
-    return (
-      <div>Hello Auth Page</div>
-    )
-  }
+class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            allImages: []
+        };
+    }
+
+    getAllArtifacts = e => {
+        const db = this.props.firebase.db;
+        const artifacts = db.collection("artifacts");
+
+        artifacts
+            .get()
+            .then(querySnapshot => {
+                querySnapshot.forEach(doc => {
+                    this.setState({
+                        allImages: [
+                            {
+                                url: doc.data().description
+                            }
+                        ]
+                    });
+                });
+            })
+            .catch(error => {
+                console.log(error);
+            });
+    };
+
+    render() {
+        return <button onClick={this.getAllArtifacts}>Hello Auth Page</button>;
+    }
 }
+
+export default withFirebase(HomePage);
