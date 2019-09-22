5aba815bb60413b6691e4eda6f21f6047486b97f
Arpit <arpitbajaj98@gmail.com>
2019-09-22 23:36:48 +1000

    Can upload images from PC

--- app/src/components/AddArtifactPage/index.jsx
+++ app/src/components/AddArtifactPage/index.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import withFirebase from "../../contexts/withFirebase";
+import Button from "react-bootstrap/Button";
 
-export default class AddArtifactPage extends React.Component {
+class AddArtifactPage extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            image: null,
+            url: ""
+        };
+    }
+
+    handleChange = e => {
+        if (e.target.files[0]) {
+            const image = e.target.files[0];
+            this.setState(() => ({
+                image
+            }));
+        }
+    };
     render() {
-        return <div>Hello Auth Page</div>;
+        return (
+            <div>
+                <input type="file" onChange={this.handleChange}></input>
+                <Button variant="primary">Image upload</Button>
+            </div>
+        );
     }
 }
+
+export default withFirebase(AddArtifactPage);
