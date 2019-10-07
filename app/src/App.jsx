b12183b55e599aaf95f14fe9dddac557445c0952
Angel <angelyuan37@gmail.com>
2019-10-07 15:51:48 +1100

    clean up formatting

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,14 +1,14 @@
-import React from 'react'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
-import 'font-awesome/css/font-awesome.css'
-import HomePage from './components/HomePage'
-import LandingPage from './components/LandingPage'
-import SignInPage from './components/SignInPage'
-import SignUpPage from './components/SignUpPage'
-import Navbar from './components/Navbar'
-import AuthRoute from './components/AuthRoute'
-import ArtifactPage from './components/ArtifactPage'
-import EditPage from './components/EditPage'
+import React from "react"
+import { BrowserRouter as Router, Route } from "react-router-dom"
+import "font-awesome/css/font-awesome.css"
+import HomePage from "./components/HomePage"
+import LandingPage from "./components/LandingPage"
+import SignInPage from "./components/SignInPage"
+import SignUpPage from "./components/SignUpPage"
+import Navbar from "./components/Navbar"
+import AuthRoute from "./components/AuthRoute"
+import ArtifactPage from "./components/ArtifactPage"
+import EditPage from "./components/EditPage"
 
 function App() {
   return (
@@ -18,9 +18,17 @@ function App() {
         <Route path="/" exact component={LandingPage} />
         <Route path="/signin" component={SignInPage} />
         <Route path="/signup" component={SignUpPage} />
-        <AuthRoute path='/home' component={HomePage} />
-        <AuthRoute path="/artifact/:artifactId" exact component={ArtifactPage} />
-        <AuthRoute path="/artifact/edit/:artifactId" exact component={EditPage} />
+        <AuthRoute path="/home" component={HomePage} />
+        <AuthRoute
+          path="/artifact/:artifactId"
+          exact
+          component={ArtifactPage}
+        />
+        <AuthRoute
+          path="/artifact/edit/:artifactId"
+          exact
+          component={EditPage}
+        />
       </Router>
     </div>
   )
