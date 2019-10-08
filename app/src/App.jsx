8a323340c46a39cd01ffb69763e7b58b5394bd9d
Arpit Bajaj <arpitbajaj98@gmail.com>
2019-10-08 18:44:58 +1100

    Importing add artifact

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -8,6 +8,7 @@ import SignInPage from "./components/SignInPage"
 import SignUpPage from "./components/SignUpPage"
 import Navbar from "./components/Navbar"
 import AuthRoute from "./components/AuthRoute"
+import AddArtifactPage from "./components/AddArtifactPage"
 import ArtifactPage from "./components/ArtifactPage"
 
 function App() {
@@ -15,12 +16,12 @@ function App() {
     <div>
       <Router>
         <Navbar />
-        <Route path="/" exact component={LandingPage} />
-        <Route path="/signin" component={SignInPage} />
-        <Route path="/signup" component={SignUpPage} />
-        <AuthRoute path="/home" component={HomePage} />
-        <AuthRoute path="/addartifact" component={AddArtifactPage} />
-        <AuthRoute path="/artifact/:artifactId" component={ArtifactPage} />
+        <Route path='/' exact component={LandingPage} />
+        <Route path='/signin' component={SignInPage} />
+        <Route path='/signup' component={SignUpPage} />
+        <AuthRoute path='/home' component={HomePage} />
+        <AuthRoute path='/addartifact' component={AddArtifactPage} />
+        <AuthRoute path='/artifact/:artifactId' component={ArtifactPage} />
       </Router>
     </div>
   )
