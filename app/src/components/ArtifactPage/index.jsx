b6dedf3afb3db5595f21b4e84f9d4314cfc3dcfc
Angel <angelyuan37@gmail.com>
2019-10-08 14:11:19 +1100

    changed image url from storage ref to actual downloadable url

--- app/src/components/ArtifactPage/index.jsx
+++ app/src/components/ArtifactPage/index.jsx
@@ -1,100 +1,103 @@
-import React from 'react'
-import withFirebase from '../../contexts/withFirebase'
-import Card from 'react-bootstrap/Card'
-import RightArrow from './RightArrow'
-import LeftArrow from './LeftArrow'
-import Spinner from './Spinner'
+import React from "react"
+import withFirebase from "../../contexts/withFirebase"
+import Card from "react-bootstrap/Card"
+import RightArrow from "./RightArrow"
+import LeftArrow from "./LeftArrow"
+import Spinner from "./Spinner"
 
 class ArtifactPage extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            artifactId: this.props.match.params.artifactId,
-            data: {},
-            isLoading: true,
-            artifactExists: false,
-            images: [],
-            imageIndex: 0,
-        }
+  constructor(props) {
+    super(props)
+    this.state = {
+      artifactId: this.props.match.params.artifactId,
+      data: {},
+      isLoading: true,
+      artifactExists: false,
+      images: [],
+      imageIndex: 0
     }
-    componentDidMount() {
-        this.fetchArtifact()
-    }
+  }
+  componentDidMount() {
+    this.fetchArtifact()
+  }
 
-    fetchArtifact = async () => {
-        const artifactId = this.state.artifactId
-        console.log("getting artifact with id: " + artifactId)
-        let artifact = this.props.firebase.db.collection('artifacts').doc(artifactId)
-
-        let imageRefUrls = []
-        try {
-            const artifactDoc = await artifact.get()
-            if (!artifactDoc.exists) {
-                this.setState( {isLoading: false} )
-                return
-            }
-            this.setState( {artifactExists: true} )
-            console.log(artifactDoc.data())
-            imageRefUrls = artifactDoc.data().image
-            console.log(imageRefUrls)
-            this.setState({ data : artifactDoc.data()})
-            
-            imageRefUrls.forEach(async (refUrl) => {
-                let imageRef = this.props.firebase.storage.refFromURL(refUrl)
-                const url = await imageRef.getDownloadURL()
-                let images = this.state.images
-                images.push(url)
-                this.setState( {images: images, isLoading: false} )
-            })
-
-        } catch (e) {
-            console.log("Error getting document:", e);
-        }
+  fetchArtifact = async () => {
+    const artifactId = this.state.artifactId
+    console.log("getting artifact with id: " + artifactId)
+    let artifact = this.props.firebase.db
+      .collection("artifacts")
+      .doc(artifactId)
+    try {
+      const artifactDoc = await artifact.get()
+      if (!artifactDoc.exists) {
+        this.setState({ isLoading: false })
+        return
+      }
+      this.setState({ artifactExists: true })
+      console.log(artifactDoc.data())
+      // speed opt here: download image first
+      this.setState({
+        data: artifactDoc.data(),
+        images: artifactDoc.data().image,
+        isLoading: false
+      })
+    } catch (e) {
+      console.log("Error getting document:", e)
     }
+  }
 
-    goToPrevSlide = () => {
-        if(this.state.imageIndex === 0) {
-            return this.setState( {imageIndex: this.state.images.length-1} )
-        }
-        this.setState({imageIndex: this.state.imageIndex - 1 })
+  goToPrevSlide = () => {
+    if (this.state.imageIndex === 0) {
+      return this.setState({ imageIndex: this.state.images.length - 1 })
     }
-    
-    goToNextSlide = () => {
-        if(this.state.imageIndex === this.state.images.length - 1) {
-          return this.setState( {imageIndex: 0} )
-        }
-        this.setState({imageIndex: this.state.imageIndex + 1 })
-    }
-    
-    buildSlide = () => {
-        return <div className="slider" style={{display: 'flex'}}>
-                <LeftArrow goToPrevSlide={this.goToPrevSlide} />
-                <div className="slide">
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src={this.state.images[this.state.imageIndex]} alt="missing" />
-                        <Card.Body>
-                            <Card.Title>{this.state.data.description}</Card.Title>
-                            <Card.Text>
-                                Created by {this.state.data.ownerId} <br />
-                                Related to {this.state.data.relatedFamilyMembers}
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                </div>
-                <RightArrow goToNextSlide={this.goToNextSlide} />
-              </div>
-    }
+    this.setState({ imageIndex: this.state.imageIndex - 1 })
+  }
 
-    render() {
-
-        return (
-            <div>
-                {this.state.isLoading && <Spinner />}
-                {this.state.artifactExists && !this.state.isLoading && this.buildSlide()}
-                {!this.state.artifactExists && !this.state.isLoading && <div>The artifact does not exsit. </div>}
-            </div>
-        )
+  goToNextSlide = () => {
+    if (this.state.imageIndex === this.state.images.length - 1) {
+      return this.setState({ imageIndex: 0 })
     }
+    this.setState({ imageIndex: this.state.imageIndex + 1 })
+  }
+
+  buildSlide = () => {
+    return (
+      <div className="slider" style={{ display: "flex" }}>
+        <LeftArrow goToPrevSlide={this.goToPrevSlide} />
+        <div className="slide">
+          <Card style={{ width: "18rem" }}>
+            <Card.Img
+              variant="top"
+              src={this.state.images[this.state.imageIndex]}
+              alt="missing"
+            />
+            <Card.Body>
+              <Card.Title>{this.state.data.description}</Card.Title>
+              <Card.Text>
+                Created by {this.state.data.ownerId} <br />
+                Related to {this.state.data.relatedFamilyMembers}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+        <RightArrow goToNextSlide={this.goToNextSlide} />
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.isLoading && <Spinner />}
+        {this.state.artifactExists &&
+          !this.state.isLoading &&
+          this.buildSlide()}
+        {!this.state.artifactExists && !this.state.isLoading && (
+          <div>The artifact does not exsit. </div>
+        )}
+      </div>
+    )
+  }
 }
 
-export default withFirebase(ArtifactPage);
+export default withFirebase(ArtifactPage)
