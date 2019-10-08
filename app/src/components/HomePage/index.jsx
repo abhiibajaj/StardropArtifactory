84102c64ed0d692091b71c438b117bbdde0c8bd0
Arpit Bajaj <arpitbajaj98@gmail.com>
2019-10-08 16:53:06 +1100

    Handling multiple images in an array

--- app/src/components/HomePage/index.jsx
+++ app/src/components/HomePage/index.jsx
@@ -1,73 +1,50 @@
-import React from "react";
-import withFirebase from "../../contexts/withFirebase";
+import React from "react"
+import withFirebase from "../../contexts/withFirebase"
 
 class HomePage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            images: []
-        };
+  constructor(props) {
+    super(props)
+    this.state = {
+      images: []
     }
+  }
 
-    componentDidMount() {
-        this.getAllArtifacts();
-        this.getImageUrl();
+  componentDidMount() {
+    this.getAllArtifacts()
+  }
+
+  getAllArtifacts = async () => {
+    const db = this.props.firebase.db
+    const artifacts = db.collection("artifacts")
+    let allImageUrls = []
+
+    try {
+      const querySnapshot = await artifacts.get()
+      querySnapshot.forEach(doc => {
+        allImageUrls = [...allImageUrls, ...doc.data().image]
+      })
+    } catch (e) {
+      console.log("Error getting document:", e)
     }
+    this.setState({
+      images: allImageUrls
+    })
+  }
 
-    getImageUrl = async imageRefUrl => {
-        try {
-            const imageRef = this.props.firebase.storage.refFromURL(
-                imageRefUrl
-            );
-            const imageUrl = await imageRef.getDownloadURL();
-            this.setState(state => {
-                const images = state.images.concat(imageUrl);
-                return {
-                    images
-                };
-            });
-        } catch (error) {
-            console.log(error);
-        }
-    };
-    getAllArtifacts = async () => {
-        const db = this.props.firebase.db;
-        const artifacts = db.collection("artifacts");
-        const allImageUrls = [];
-
-        try {
-            const querySnapshot = await artifacts.get();
-            querySnapshot.forEach(doc => {
-                allImageUrls.push(doc.data().image);
-            });
-        } catch (e) {
-            console.log("Error getting document:", e);
-        }
-        allImageUrls.forEach(imageRefUrl => {
-            this.getImageUrl(imageRefUrl);
-        });
-    };
-
-    render() {
-        return (
-            <div>
-                {[...this.state.images].map((image, index) => {
-                    console.log(image);
-                    console.log(index);
-                    return (
-                        <img
-                            key={index}
-                            height="250"
-                            width="250"
-                            alt=""
-                            src={image}
-                        ></img>
-                    );
-                })}
-            </div>
-        );
-        //return <button onClick={this.getAllArtifacts}>Hello Auth Page</button>;
-    }
+  render() {
+    return (
+      <div>
+        {[...this.state.images].map((image, index) => {
+          console.log(image)
+          console.log(index)
+          return (
+            <img key={index} height='250' width='250' alt='' src={image}></img>
+          )
+        })}
+      </div>
+    )
+    //return <button onClick={this.getAllArtifacts}>Hello Auth Page</button>;
+  }
 }
 
-export default withFirebase(HomePage);
+export default withFirebase(HomePage)
