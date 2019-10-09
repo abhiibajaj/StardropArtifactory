4d449d9abaac2991bcb5bbee76250762adfdc23b
Matthew Littleson <matt@lookatmedam.com>
2019-10-09 18:48:13 +1100

    updated navbar styling

--- app/src/components/Navbar/index.jsx
+++ app/src/components/Navbar/index.jsx
@@ -2,53 +2,57 @@ import React from "react"
 import { Link } from "react-router-dom"
 import withAuth from "../../contexts/withAuth"
 import withFirebase from "../../contexts/withFirebase"
-import { Navbar, Nav } from "react-bootstrap"
+import { Menu, Segment, Dropdown, Icon } from 'semantic-ui-react'
 import Logout from "./Logout"
 import "./index.css"
 
-const NavLinks = ({ auth, firebase }) => (
-  <div>
-    {auth.loggedIn ? (
-      <Nav className='justify-content-end'>
-        <Navbar.Text>
-          <span className='user-display'>
-            {auth.loggedIn ? auth.data.email : ""}
-          </span>
-        </Navbar.Text>
-        <Logout firebase={firebase} />
-      </Nav>
-    ) : (
-      <Nav className='justify-content-end'>
-        <Nav.Item>
-          <Link to='/signin'>
-            <span className='nav-link-button'>Sign In</span>
-          </Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Link to='/signup'>
-            <span className='nav-link-button'>Sign Up</span>
-          </Link>
-        </Nav.Item>
-      </Nav>
-    )}
-  </div>
-)
-
 const TopNavbar = ({ auth, firebase }) => (
-  <Navbar collapseOnSelect expand='lg' bg='light'>
-    <Navbar.Brand>
-      <Link to='/home'>
-        <span className='logo-text'>Stardrop</span>
-      </Link>
-      <Link to='/addartifact'>
-        <span className='logo-text'> Add Artifact</span>
-      </Link>
-    </Navbar.Brand>
-    <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-    <Navbar.Collapse className='justify-content-end'>
-      <NavLinks auth={auth} firebase={firebase} />
-    </Navbar.Collapse>
-  </Navbar>
+  <Segment style={{ borderRadius: 0, margin: 0, padding: 0 }}>
+    <Menu secondary style={{ margin: 0, padding: 0 }}>
+      <Menu.Item>
+        <Link to='/home'>
+          <img alt='home' height="32px" width="32px" src='/logo.png' />
+        </Link>
+      </Menu.Item>
+
+      {auth.loggedIn ? (
+        <Menu.Menu position='right'>
+          <Menu.Item name='addartifact'>
+            <Link to='/addartifact'>
+              <Icon name='plus square outline' fitted size='big' color='violet' />
+            </Link>
+          </Menu.Item>
+          <Menu.Item name='search'>
+            <Link to='/home'>
+              <Icon name='th' fitted size='big' color='violet' />
+            </Link>
+          </Menu.Item>
+          <Dropdown item text={auth.loggedIn ? auth.data.email : ""}>
+            <Dropdown.Menu>
+              <Dropdown.Item>
+                <Logout firebase={firebase} />
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Menu.Menu>
+      ) : (
+          <Menu.Menu position='right'>
+            <Menu.Item>
+              <Menu.Item>
+                <Link to='/signin'>
+                  Sign In
+              </Link>
+              </Menu.Item>
+              <Menu.Item>
+                <Link to='/signup'>
+                  Sign Up
+              </Link>
+              </Menu.Item>
+            </Menu.Item>
+          </Menu.Menu>
+        )}
+    </Menu>
+  </Segment>
 )
 
 export default withAuth(withFirebase(TopNavbar))
