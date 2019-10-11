9bfe2e9da5e5da3b8c32df4816b485fc13b1c3d5
Matt Littleson <matt.littleson@gmail.com>
2019-10-12 00:24:03 +1100

    added tv icon for slideshow page

--- app/src/components/Navbar/index.jsx
+++ app/src/components/Navbar/index.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "react-router-dom"
 import withAuth from "../../contexts/withAuth"
 import withFirebase from "../../contexts/withFirebase"
-import { Menu, Segment, Dropdown, Icon } from 'semantic-ui-react'
+import { Menu, Segment, Dropdown, Icon } from "semantic-ui-react"
 import Logout from "./Logout"
 import "./index.css"
 
@@ -10,21 +10,31 @@ const TopNavbar = ({ auth, firebase }) => (
   <Segment style={{ borderRadius: 0, margin: 0, padding: 0 }}>
     <Menu secondary style={{ margin: 0, padding: 0 }}>
       <Menu.Item>
-        <Link to='/home'>
-          <img alt='home' height="32px" width="32px" src='/logo.png' />
+        <Link to="/home">
+          <img alt="home" height="32px" width="32px" src="/logo.png" />
         </Link>
       </Menu.Item>
 
       {auth.loggedIn ? (
-        <Menu.Menu position='right'>
-          <Menu.Item name='addartifact'>
-            <Link to='/addartifact'>
-              <Icon name='plus square outline' fitted size='big' color='violet' />
+        <Menu.Menu position="right">
+          <Menu.Item name="search">
+            <Link to="/home">
+              <Icon name="home" fitted size="large" color="violet" />
             </Link>
           </Menu.Item>
-          <Menu.Item name='search'>
-            <Link to='/home'>
-              <Icon name='th' fitted size='big' color='violet' />
+          <Menu.Item name="addartifact">
+            <Link to="/addartifact">
+              <Icon
+                name="plus square outline"
+                fitted
+                size="big"
+                color="violet"
+              />
+            </Link>
+          </Menu.Item>
+          <Menu.Item name="slideshow">
+            <Link to="/slideshow">
+              <Icon name="tv" fitted size="large" color="violet" />
             </Link>
           </Menu.Item>
           <Dropdown item text={auth.loggedIn ? auth.data.email : ""}>
@@ -36,21 +46,17 @@ const TopNavbar = ({ auth, firebase }) => (
           </Dropdown>
         </Menu.Menu>
       ) : (
-          <Menu.Menu position='right'>
+        <Menu.Menu position="right">
+          <Menu.Item>
             <Menu.Item>
-              <Menu.Item>
-                <Link to='/signin'>
-                  Sign In
-              </Link>
-              </Menu.Item>
-              <Menu.Item>
-                <Link to='/signup'>
-                  Sign Up
-              </Link>
-              </Menu.Item>
+              <Link to="/signin">Sign In</Link>
             </Menu.Item>
-          </Menu.Menu>
-        )}
+            <Menu.Item>
+              <Link to="/signup">Sign Up</Link>
+            </Menu.Item>
+          </Menu.Item>
+        </Menu.Menu>
+      )}
     </Menu>
   </Segment>
 )
